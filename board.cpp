472505753cca940dd5f63fdffd269e7d2fa33573
Kayley Kiesel <kayleykiesel@Kayleys-MBP.home>
2018-11-30 13:42:54 -0500

    added logic for players making another turn if capturing a piece

--- board.cpp
+++ board.cpp
@@ -3,6 +3,7 @@
 class board {
     space*** checkerboard;
     public:
+    // directions based on the perspective of player's side of the board.
     enum movement {LEFT, RIGHT, NONE};
     movement isJumping;
     user *user1, *user2, *currUser, *otherUser;
@@ -54,12 +55,9 @@ class board {
     bool isPieceValid(short y1, short x1) {
         // ignoring kings for now...
         // if the selected squre is part of the board (so as not to get seg fault).
-        std:: cout << "9 ";
         if (y1 >= 0 && y1 < 8 && x1 >= 0 && x1 < 8) {
-            std:: cout << "9 ";
             // if their color piece was chosen
             if (checkerboard[y1][x1] != NULL && checkerboard[y1][x1]->getOwner() == currUser) {
-                std:: cout << "9 ";
                 // if user is B. (can move any normal piece forwards. if at end it is a king)
                 return true;
             }
@@ -68,42 +66,25 @@ class board {
     }
     bool isNextPosValid(short y1, short x1, short y2, short x2, short k) {
         isJumping = NONE;
-        std:: cout << "9 ";
         // check if in bounds of array.
         if (y2 >= 0 && y2 < 8 && x2 >= 0 && x2 < 8) {
-            std:: cout << "9 ";
-            std::cout<<"is in array";
-            // if the space object exists here, and there is no owner of the space.
+            // if the chosen location is a real space and there is no owner of the space.
             if (checkerboard[y2][x2] != NULL && checkerboard[y2][x2]->getOwner() == NULL) {
-                std::cout<<"space exists, no owner of space";
-                // pieces move down
-                    // moving left OR right one space.
-                    if (abs(x1 - x2) == 1) {
-                        // down one space
-                        if (y2 == (y1 + k)) {
-                            return true;
-                        }
-                        // for both useres, trying to jump.
-                    } else if ((y1 - y2) == -2 * k) {
-                        // if moving right
-                        if (((x2 - x1) == 2) && checkerboard[y1 + k][x1 + 1]->getOwner() == otherUser) {
-                            isJumping = RIGHT;
-                            return true;
-                            // if moving left.
-                        } else if (((x2 - x1) == -2) && checkerboard[y1 + k][x1 - 1]->getOwner() == otherUser) {
-                            isJumping = LEFT;
-                            return true;
-                        }
-                            // WORKING ON THIS PART!!! JUMPING A PIECE. CHECKING IF SPACE BETWEEN IS HELD BY OPPOSITE USER.
-                    }
-                /*} else { // is user2
-                    std::cout<<"isplayer2";
-                    // if moving down one space, and left OR right one space.
-                    if (y2 == (y1 - 1) && (x2 == (x1 - 1) || (x2) == (x1 + 1))) {
-                        std::cout<<"moving down and over 1";
+                // moving left OR right one space, and up/down depending on user
+                if (abs(x1 - x2) == 1 && y2 == (y1 + k)) {
+                        return true;
+                // trying to jump. (two spaces up/down depending on user)
+                } else if ((y1 - y2) == -2 * k) {
+                    // moving right, checks if piece being captured is owned by other user
+                    if (((x2 - x1) == 2) && checkerboard[y1 + k][x1 + 1]->getOwner() == otherUser) {
+                        setJumping(RIGHT);
+                        return true;
+                        // if moving left, checks if piece being captured is owned by other user
+                    } else if (((x2 - x1) == -2) && checkerboard[y1 + k][x1 - 1]->getOwner() == otherUser) {
+                        setJumping(LEFT);
                         return true;
                     }
-                }*/
+                }
             }
         }
         return false;
@@ -111,9 +92,7 @@ class board {
 
     bool isMoveValid(short y1, short x1, short y2, short x2, short k) {
         if (isPieceValid(y1, x1)) {
-            std::cout<<"piece is valid";
             if (isNextPosValid(y1, x1, y2, x2, k)) {
-                std::cout << "pos is valid";
                 return true;
             }
         }
@@ -124,24 +103,28 @@ class board {
         checkerboard[y2][x2]->setOwner(currUser);
         if (getJumping() == RIGHT) {
             checkerboard[y1 + k][x1 + 1]->setOwner(NULL);
-        } else if (isJumping == LEFT) {
+        } else if (getJumping() == LEFT) {
             checkerboard[y1 + k][x1 - 1]->setOwner(NULL);
         }
         std::cout << currUser->getOwner();
+        print();
     }
     void start() {
-        short y1, x1, y2, x2;
+        short y1, x1, y2, x2, y3, x3;
         char a;
         char isAnotherMove = 'N';
-        bool isMoveValid = true;
+        bool isMoveValid = true, jumpedAPiece = false;
         currUser = user2;
         otherUser = user1;
         std::cout << user1->getOwner() << ", your piece is: " << user1->getPiece() << " your king is: " << user1->getKing() << "\n";
         std::cout << user2->getOwner() << ", your piece is: " << user2->getPiece() << " your king is: " << user2->getKing() << "\n";
         std::cout << "ready to start? press any key to continue and hit enter.";
         std:: cin >> a;
+        print();
          while(true) {
-            print();
+            short turnCount = 0;
+            jumpedAPiece = false;
+            isMoveValid = true;
             setJumping(NONE);
             // switch to next player's turn;
             currUser = currUser == user1 ? user2 : user1;
@@ -151,38 +134,65 @@ class board {
             std::cout << currUser->getOwner() << "'s turn\n";
             do {
                 isAnotherMove = 'N';
-                std::cout << "choose a row (1-8)";
+                std::cout << "choose a row (1-8): ";
                 std::cin >> y1;
-                std::cout << "choose a column";
+                std::cout << "choose a column: ";
                 std::cin >> x1;
-                do {
-                    std::cout << "choose the row you want the piece to move";
-                    std::cin >> y2;
-                    std::cout << "choose the column";
-                    std::cin >> x2;
-
+                std::cout << "choose the row you want the piece to move: ";
+                std::cin >> y2;
+                std::cout << "choose the column: ";
+                std::cin >> x2;
+                y1--;
+                x1--;
+                y2--;
+                x2--;
+            do {
+                if (turnCount > 0 && jumpedAPiece) {
+                    std::cout << "choose the row you want the piece to move: ";
+                    std::cin >> y3;
+                    std::cout << "choose the column: ";
+                    std::cin >> x3;
                     // convert from 1 - 8 to 0 - 7.
-                    y1--;
-                    x1--;
-                    y2--;
-                    x2--;
+                    y3--;
+                    x3--;
 
-                    if (this->isMoveValid(y1, x1, y2, x2, k)) {
-                        movePiece(y1, x1, y2, x2, k);
-                        if (getJumping() != NONE) {
-                            std::cout << "jump again? Y or N";
-                            std :: cin >> isAnotherMove;
-                        }
+                    // if not first turn, previously jumped, and next pos is valid
+                    if (this->isNextPosValid(y2, x2, y3, x3, k)) {
                         isMoveValid = true;
+                        movePiece(y2, x2, y3, x2, k);
+                        std::cout << "you captured a piece! jump again? Y/N: ";
+                        std::cin >> isAnotherMove;
+                        turnCount++;
                     } else {
+                        std::cout << "invalid try again? Y/N: ";
+                        std::cin >> isAnotherMove;
+                    }
+                } else if (this->isMoveValid(y1, x1, y2, x2, k)) {
+                    isMoveValid = true;
+                    // if their first move, can move or jump.
+                    if (turnCount == 0) {
+                        turnCount++;
+                        movePiece(y1, x1, y2, x2, k);
+                        // if jumped, can jump again.
+                        if (getJumping() != NONE) {
+                            std::cout << "you captured a piece! jump again? Y/N: ";
+                            std::cin >> isAnotherMove;
+                            // set first move to a jump.
+                            jumpedAPiece = true;
+                        }
+                        // if not first move then they must jump again.
+                    }
+                        // if jumped first move and move invalid, choose to jump again
+                } else if (jumpedAPiece) {
+                    std::cout << "invalid. Try again? Y/N: ";
+                    std::cin >> isAnotherMove;
+                    // first move invalid, has to move again.
+                } else {
                         isMoveValid = false;
-                        std:: cout << "invalid. try again";
-                        print();
+                        std::cout<<"move is invalid. try again.";
                     }
                 } while (isAnotherMove == 'Y' || isAnotherMove == 'y');
             } while(!isMoveValid);
-
         }
-
     }
 };
--- checkers.cpp
+++ checkers.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 int main () {
     string name;
-    cout << "player 1 enter your name";
+    cout << "player 1 enter your name: ";
     cin >> name;
     user user1('B', '&', name);
-    cout << "player 2 enter your name";
+    cout << "player 2 enter your name: ";
     cin >> name;
     user user2('W', '#', name);
     board board1(&user1, &user2);
